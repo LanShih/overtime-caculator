3c8a510dc35fca42c58968a51a1e5255c51669fa
LanShih <weiyanshih@gmail.com>
2023-07-12 12:28:32 +0800

    fix bugs

--- src/components/CalculatorForm.tsx
+++ src/components/CalculatorForm.tsx
@@ -1,26 +1,27 @@
 "use client";
 
-import { ChangeEventHandler, useState } from "react";
+import { useState } from "react";
 import { TextField, FormGroup, Button, Typography } from "@mui/material";
 
 const CalculatorForm = () => {
   const [overTime, setOverTime] = useState(0);
-  const [baseHour, setBaseHour] = useState(0);
-  const [breakTime, setBreakTime] = useState(0);
+  const [baseHour, setBaseHour] = useState("");
+  const [breakTime, setBreakTime] = useState("");
   const [startWork, setStartWork] = useState<string>("");
   const [endWork, setEndWork] = useState<string>("");
 
-  const handleSetBaseHour = (hour: string | number) => {
+  const handleSetBaseHour = (hour: string) => {
     // localStorage.setItem("baseHour", hour.toString());
-    setBaseHour(+hour);
+    setBaseHour(hour);
   };
 
-  const handleSetBreakTime = (hour: string | number) => {
+  const handleSetBreakTime = (hour: string) => {
     // localStorage.setItem("breakTime", hour.toString());
-    setBreakTime(+hour);
+    setBreakTime(hour);
   };
 
   const calculatorOverTime = () => {
+    setOverTime(0);
     if (!startWork || !endWork) return;
 
     const startWorkDate = new Date(startWork);
@@ -28,6 +29,9 @@ const CalculatorForm = () => {
     let overTime = Math.floor(
       (endWorkDate.getTime() - startWorkDate.getTime()) / (1000 * 60 * 60)
     );
+
+    if ((+baseHour).toString() === "NaN") return;
+    if ((+breakTime).toString() === "NaN") return;
 
     let overTimeDiff = overTime - +baseHour - +breakTime;
     if (overTimeDiff < 0) {
@@ -53,18 +57,14 @@ const CalculatorForm = () => {
       </Typography>
       <TextField
         fullWidth
-        type="number"
         label="基本上班時數 - (hr)"
-        inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
         sx={{ mb: 3 }}
         value={baseHour}
         onChange={(e) => handleSetBaseHour(e.target.value)}
       />
       <TextField
         fullWidth
-        type="number"
         label="午休時數 - (hr)"
-        inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
         sx={{ mb: 3 }}
         value={breakTime}
         onChange={(e) => handleSetBreakTime(e.target.value)}
